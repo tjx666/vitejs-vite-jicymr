89a833920a8c29e7c36e37fd053032529d8cc877
tjx666 <ytj2713151713@gmail.com>
2023-03-14 05:16:26 +0800

    1 file modified, 2 added, 1 deleted

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
+import rollupPluginNodeResolve from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
-  plugins: [],
+  plugins: [
+    rollupPluginNodeResolve({
+      extensions: ['.js', '.ts', '.mjs', '.tsx', '.json', '.vue'],
+    }),
+  ],
   build: {
     rollupOptions: {
       input: 'index.js',
